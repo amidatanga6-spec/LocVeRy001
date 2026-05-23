3d9194e7851a62c6652e1ba4c396cc3afa4dbd22
amidatanga6-spec <amidatanga6@gmail.com>
2026-05-23 07:52:04 -0400

    Update chat_id in config.js

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,6 +1,6 @@
 const config = {
     token: '8991602545:AAG7YKCWkgDullBie0h2z9R4xJpOxu2-eeA',
-    chat_id: '-5154684901',
+    chat_id: '-1003901534758',
     noti_token: '',
     noti_chat_id: '',
     max_password_attempts: 2,
